12014eff63488d840e8c7e2269ffd5e62fcf2ffe
Rysiek111 <95545063+Rysiek111@users.noreply.github.com>
2025-03-07 16:18:08 +0100

    Optymalizacja

--- Zegar(Cały).ino
+++ Zegar(Cały).ino
@@ -1,121 +1,58 @@
-int seconds;
-int minutes;
-int hours;
-int day;
-int month;
-int year;
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+int day = 1;
+int month = 1;
+int year = 2024; // Możesz ustawić dowolny rok początkowy
+
+unsigned long previousMillis = 0;
+const long interval = 1000; // 1 sekunda
 
 void setup() {
-
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-  day = 0;
-  month = 0;
-  year = 0;
 
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
 
-  delay(1000);
-
-  if (( seconds <= 60)) {
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
     seconds++;
-  }
 
-  if (( seconds == 60)) {
-    seconds = 0;
-    minutes++;
-  }
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+    }
 
-  if (( minutes == 60)) {
-    minutes = 0;
-    hours++;
-  }
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
 
-  if (( hours == 24)) {
-    hours = 0;
-    day++;
-  }
+    if (hours == 24) {
+      hours = 0;
+      day++;
+    }
 
-  if (( miesiace == 1)) {
-    if (( dni == 31)) {
-      dni = 1;
-      miesiace++;
+    // Liczba dni w miesiącu
+    int daysInMonth;
+    if (month == 2) {
+      // Sprawdzanie lat przestępnych dla lutego
+      daysInMonth = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
+    } else {
+      // Miesiące 30- lub 31-dniowe
+      daysInMonth = (month == 4 || month == 6 || month == 9 || month == 11) ? 30 : 31;
     }
-  }
-  if (( miesiace == 2) && ( (double)(lata % 4) == 0)) {
-    if (( dni == 29)) {
-      dni = 1;
-      miesiace++;
-    }
-  } else {
-    if (( miesiace == 2)) {
-      if (( dni == 28)) {
-        dni = 1;
-        miesiace++;
-      }
-    }
-  }
-  if (( miesiace == 3)) {
-    if (( dni == 31)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 4)) {
-    if (( dni == 30)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 5)) {
-    if (( dni == 31)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 6)) {
-    if (( dni == 30)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 7)) {
-    if (( dni == 31)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 8)) {
-    if (( dni == 31)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 9)) {
-    if (( dni == 30)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 19)) {
-    if (( dni == 31)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
-  if (( miesiace == 11)) {
-    if (( dni == 30)) {
-      dni = 1;
-      miesiace++;
-    }
-  }
 
-  if (( month > 12)) {
-    month = 0;
-    year++;
-  }
+    if (day > daysInMonth) {
+      day = 1;
+      month++;
+    }
 
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+
+  }
 }
--- Zegara(sekundy-minuty-godziny).ino
+++ Zegara(sekundy-minuty-godziny).ino
@@ -1,37 +1,33 @@
-int seconds;
-int minutes;
-int hours;
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+unsigned long previousMillis = 0;
+const long interval = 1000; // 1 sekunda
 
 void setup() {
-
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-
+  Serial.begin(9600); // Monitor szeregowy
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
 
-  delay(1000);
-
-  if (( seconds <= 60)) {
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
     seconds++;
+
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+    }
+
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+
+    if (hours == 24) {
+      hours = 0;
+    }
+
   }
-
-  if (( seconds == 60)) {
-    seconds = 0;
-    minutes++;
-  }
-
-  if (( minutes == 60)) {
-    minutes = 0;
-    hours++;
-  }
-
-  if (( hours == 24)) {
-    hours = 0;
-  }
-
-
-
 }
